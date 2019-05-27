56cb02a2c1701fe5db0b2e21ba11ede341eea1da
iningwei <iningwei@outlook.com>
2019-05-27 16:35:46 +0800

    add PadLeft()

--- Extentions/StringExt.ts
+++ Extentions/StringExt.ts
@@ -1,6 +1,11 @@
 export default class StringExt {
-    public static Format(inputStr: string, ...args: any[]): string {
-        let result = inputStr;
+    /**
+     * a target string like this:my name is {0},age is {1}
+     * @param target the target to be formated
+     * @param args 
+     */
+    public static Format(target: string, ...args: any[]): string {
+        let result = target;
         if (args.length > 0) {
             let value: any;
             for (const key in args) {
@@ -13,6 +18,27 @@ export default class StringExt {
                 }
             }
         }
+        return result;
+    }
+
+    /**
+     * 
+     * @param target 
+     * @param totalLen 
+     * @param paddingChar 
+     */
+    public static PadLeft(target: any, totalLen: number, paddingChar: any = "0"):string {
+        if (String(paddingChar).length != 1) {
+            console.error("error,paddingChar should only have one char");
+        }
+        let result = String(target);
+        let len = result.length;
+        if (len < totalLen) {
+            for (let i = 0; i < totalLen - len; i++) {
+                result = paddingChar + result;
+            }
+        }
+
         return result;
     }
 }
