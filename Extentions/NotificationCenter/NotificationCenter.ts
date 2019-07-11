6a493db13ac67c3ad412a38aa1889bda07f60f20
iningwei <iningwei@outlook.com>
2019-07-11 18:25:45 +0800

    Rename NotificationName to NotificationType

--- Extentions/NotificationCenter/NotificationCenter.ts
+++ Extentions/NotificationCenter/NotificationCenter.ts
@@ -1,4 +1,6 @@
-import { NotificationName } from "./NotificationName";
+import { NotificationType } from "./NotificationType";
+
+ 
 
 /**
  * CocosCreator's global Event/Message Center.
@@ -16,26 +18,36 @@ export class NotificationCenter {
 
     /**
      * Listen to a notification
-     * @param name 
+     * @param type 
      * @param callback 
      */
-    public on(name: NotificationName, callback: Function, target?: any) {
-        this.eventTarget.on(name, callback, target);
+    public on(type: NotificationType, callback: Function, target?: any) {
+        this.eventTarget.on(type, callback, target);
     }
 
     /**
-     * Dispatch a notification
-     * @param name 
+     * 
+	 *	通过事件名发送自定义事件
+	 *	@param type event type
+	 *	@param arg1 First argument
+	 *	@param arg2 Second argument
+	 *	@param arg3 Third argument
+	 *	@param arg4 Fourth argument
+	 *	@param arg5 Fifth argument
      */
-    public emit(name: NotificationName, arg1?: any, arg2?: any, arg3?: any, arg4?: any, arg5?: any) {
-        this.eventTarget.emit(name, arg1, arg2, arg3, arg4, arg5);
+    public emit(type: NotificationType, arg1?: any, arg2?: any, arg3?: any, arg4?: any, arg5?: any) {
+        this.eventTarget.emit(type, arg1, arg2, arg3, arg4, arg5);
     }
 
     /**
-     * Cancel listen
-     * @param name 
+     * 
+     * 删除之前用同类型，回调，目标或 useCapture 注册的事件监听器，如果只传递 type，将会删除 type 类型的所有事件监听器。
+	 *	@param type A string representing the event type being removed.
+	 *	@param callback The callback to remove.
+	 *	@param target The target (this object) to invoke the callback, if it's not given, only callback without target will be removed
+	 *	
      */
-    public off(name: NotificationName, callback?: Function, target?: any) {
-        this.eventTarget.off(name, callback, target);
+    public off(type: NotificationType, callback?: Function, target?: any) {
+        this.eventTarget.off(type, callback, target);
     }
 }
--- Extentions/NotificationCenter/NotificationName.ts
+++
@@ -1,3 +0,0 @@
-export enum NotificationName {
-    BattleOver = "BattleOver"
-}
